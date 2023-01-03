7f845a7cf41a03602c24b0f5726cac40d1ca961e
srinivas <srinivas@singular.dev>
2023-01-03 15:05:56 +0530

    main branch changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_061434) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_01_03_072956) do
->>>>>>> adding_users
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,13 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_072956) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
