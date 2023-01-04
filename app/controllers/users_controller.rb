b20e8e8bca46db20960fcd8d4bab4b2d8616b503
srinivas <srinivas@singular.dev>
2023-01-04 13:44:14 +0530

    Added create and edit for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,31 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to AlphaBlog #{@user.username}, your account created successfully."
+      redirect_to articles_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Account information updated."
+      redirect_to articles_path
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_many :articles
   validates :username,
-   presence: true, uniqueness: { case_sensitive: false },
-   length: { minimum: 8, maximum: 25 }
-   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+            presence: true, uniqueness: { case_sensitive: false },
+            length: { minimum: 8, maximum: 25 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
-   presence: true, uniqueness: { case_sensitive: false },
-   length: { maximum: 105 },
-   format: {with: VALID_EMAIL_REGEX}
-   has_secure_password
+            presence: true, uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
